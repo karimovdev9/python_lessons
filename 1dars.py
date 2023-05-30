251f7789dea0f1684405c73730fb89e5a3b518d7
Abuzar <134796693+karimovdev9@users.noreply.github.com>
2023-05-30 20:39:10 +0500

    Add files via upload

---
+++ 1dars.py
@@ -0,0 +1,91 @@
+#print("qalesan")
+
+#print("Assalomu aleykum")
+ 
+# "" va '' faqri 
+
+#"" bu orqali O' va g' so'zlarini bemalol yozamiz va qo'shtirnoq 'ichida' bemalol yoza olamiz masalan
+
+# print("O'zbegim")
+# print('O'zbegim')
+# print(" Barchangizni 'Xotira va qadirlash kuni bilan tabriklayman'")
+# print(' Barchangizni 'Xotira va qadirlash kuni bilan tabriklayman'')
+
+
+
+#           ARIFMETIK AMALARLAR
+
+# + qo'shish
+#print(9+2)
+
+#- ayitish
+#print(7-3)
+
+# * ko'paytirish
+#print(9*2)
+
+#/ bolish
+#print(9/3)
+
+#//bo'lish va butun qismini olish 
+#print(7689//7)
+
+#** daraja, ildiz
+#print(9**2)
+
+#% bo'linmani qoldig'ini olish
+#print(9%5)
+
+
+
+
+
+#endi yozuv hamda arifmetik amalar
+
+# print("To'qizni kvadrati", 9**2, "ga teng")
+
+
+
+# vazifa 
+#  1. 8 ning kvadrati nechi ?
+#  2. 8 ni 7 ga bo'lganda nechi qoldiq qoladi
+
+
+
+# O'ZGARUVCHILAR(Variable)
+
+#O'zgaruvchi bu quti
+
+# ism = "Sardor"
+# yosh = 25
+# print(ism)
+# print(yosh)
+  
+# QOIDA: O'zgaruvchi nomi harf yoki _ bilan boshlanishi kere
+# o'zgaruvchi nomi raqam bilan boshlanmaydi
+#O'zgaruvchida probel bo'midi
+#o'zgaruvchida katta hamda kichik harflarni har xil o'qidi masalan Ism o'zgaruvchisi boshqa ism o'zgaruvchisi boshqa
+
+
+#string (matn)lar bilan ishlash 
+
+
+#matnlar bilan ishladi,smaylar,harflar yani boshqa tildagi harflar bilan massalan
+# ism = "sardor"
+# familya = "sodirov"
+# print(ism + ' ' + familya  )
+
+#unicode
+
+
+#uyga vazifa 
+
+#1. yengilik, ism familya sorash 
+#2. klennti yashash, tumani,  va nechinchi yil tugilgan,  va yashash manzili va yoshini chiqarverasuila
+
+ 
+
+# manzil = input("Qayerda yashaysiz >>>>>")
+# yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# yosh = 2023 - yil # 
+# print(manzil +  "Siz " + str(yosh) + " yoshda ekansiz")
